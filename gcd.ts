cbfc1de0eb722171db4da0ffe6a91df9db1a9b4e
Sarah <sarahchristen74@gmail.com>
2026-03-27 17:36:49 +0100

    aufgabe 2

--- gcd.ts
+++ gcd.ts
@@ -1,11 +1,23 @@
-export class Gcd {
-    gcdBruteForce(a: number, b: number) {
-        let gcd = 1
-        for (let i = 1; i <= Math.min(a, b); i++) {
-            if (a % i == 0 && b % i == 0) {
-                gcd = i
-            }
-        }
-        return gcd
+export function gcdBruteForce(a: number, b: number) {
+  let gcd = 1;
+  for (let i = 1; i <= Math.min(a, b); i++) {
+    if (a % i == 0 && b % i == 0) {
+      gcd = i;
     }
+  }
+  return gcd;
+}
+
+export class Fraction {
+  constructor (numerator: number, denominator: number) {
+    this.numerator = numerator
+    this.denominator = denominator
+  }
+  numerator = 0;
+  denominator = 0;
+  cancel() {
+    const gcd = gcdBruteForce(this.numerator, this.denominator);
+    this.numerator /= gcd
+    this.denominator /= gcd
+  }
 }
--- gcd_test.ts
+++ gcd_test.ts
@@ -1,12 +1,11 @@
 import { assertEquals } from "@std/assert";
-import { Gcd } from "./gcd.ts";
+import { gcdBruteForce, Fraction } from "./gcd.ts";
 
 Deno.test("ggT 1, 1 equals 1", () => {
     // Arrange
-    const gcd = new Gcd()
 
     // Act
-    const actual = gcd.gcdBruteForce(1, 1);
+    const actual = gcdBruteForce(1, 1);
 
     // Assert
     assertEquals(actual, 1);
@@ -14,10 +13,9 @@ Deno.test("ggT 1, 1 equals 1", () => {
 
 Deno.test("ggT 2, 4 equals 2", () => {
     // Arrange
-    const gcd = new Gcd()
 
     // Act
-    const actual = gcd.gcdBruteForce(2, 4);
+    const actual = gcdBruteForce(2, 4);
 
     // Assert
     assertEquals(actual, 2);
@@ -25,11 +23,68 @@ Deno.test("ggT 2, 4 equals 2", () => {
 
 Deno.test("ggT 15, 12 equals 3", () => {
     // Arrange
-    const gcd = new Gcd()
 
     // Act
-    const actual = gcd.gcdBruteForce(15, 12);
+    const actual = gcdBruteForce(15, 12);
 
     // Assert
     assertEquals(actual, 3);
+});
+
+Deno.test("ggT 14, 3 equals 1", () => {
+    // Arrange
+
+    // Act
+    const actual = gcdBruteForce(14, 3);
+
+    // Assert
+    assertEquals(actual, 1);
+});
+
+Deno.test("gekürzter Bruch 1/1 equals 1/1", () => {
+    // Arrange
+    const fraction = new Fraction(1, 1)
+
+    // Act
+    fraction.cancel();
+
+    // Assert
+    assertEquals(fraction.numerator, 1);
+    assertEquals(fraction.denominator, 1);
+});
+
+Deno.test("gekürzter Bruch 3/3 equals 1/1", () => {
+    // Arrange
+    const fraction = new Fraction(3, 3)
+
+    // Act
+    fraction.cancel();
+
+    // Assert
+    assertEquals(fraction.numerator, 1);
+    assertEquals(fraction.denominator, 1);
+});
+
+Deno.test("gekürzter Bruch 24/16 equals 3/2", () => {
+    // Arrange
+    const fraction = new Fraction(24, 16)
+
+    // Act
+    fraction.cancel();
+
+    // Assert
+    assertEquals(fraction.numerator, 3);
+    assertEquals(fraction.denominator, 2);
+});
+
+Deno.test("gekürzter Bruch 14/3 equals 14/3", () => {
+    // Arrange
+    const fraction = new Fraction(14, 3)
+
+    // Act
+    fraction.cancel();
+
+    // Assert
+    assertEquals(fraction.numerator, 14);
+    assertEquals(fraction.denominator, 3);
 });
